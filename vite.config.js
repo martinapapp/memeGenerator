1548285c1b6de91a28b31f0b5726cee32fa83dab
Martina Papp <martinapapp0@gmail.com>
2026-02-28 10:46:24 +0100

    add and test coverage (vitest-istanbul)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import {defineConfig} from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import { coverageConfigDefaults } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
+  plugins: [react()],
+  test: {
     setupFiles: ["./test-setup.js"],
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+      exclude: ['./src/index.jsx', ...coverageConfigDefaults.exclude]
+    }
   }
 })
