171f7f2d1bab0c96308199383d7b5823195e41cc
Martina Papp <martinapapp0@gmail.com>
2026-02-28 10:28:41 +0100

    add mock external service

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
     const [meme, setMeme] = useState({
         topText: "One does not simply",
         bottomText: "Walk into Mordor",
-        imageUrl: "http://i.imgflip.com/1bij.jpg"
+        imageUrl: "https://i.imgflip.com/1bij.jpg"
     })
     const [allMemes, setAllMemes] = useState([])
     
@@ -56,7 +56,7 @@ export default function Main() {
                 <button onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src={meme.imageUrl} />
+                <img src={meme.imageUrl} crossOrigin="anonymous"/>
                 <span className="top">{meme.topText}</span>
                 <span className="bottom">{meme.bottomText}</span>
             </div>
