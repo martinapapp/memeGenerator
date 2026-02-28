0ec13eacce5490e09b9975876922064d136756f2
Martina Papp <martinapapp0@gmail.com>
2026-02-28 10:07:28 +0100

    add test setup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-	plugins: [
-		react()
-	]
+	plugins: [react()],
+	test: {
+    setupFiles: ["./test-setup.js"],
+    environment: 'jsdom'
+  }
 })
